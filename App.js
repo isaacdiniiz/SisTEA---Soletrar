300e4c4c81980ed919d5c6926321b34744f75a40
isaacdiniiz <isacpdalves@gmail.com>
2023-11-25 14:32:37 -0300

    Add new image asset: imagecat.png

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
+import imagecat from './assets/imagecat.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={imagecat} style={styles.image}/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    image: {
+      width: 267,
+      height: 267,
+      flexShrink: 0,
+      resizeMode: 'contain'
+    },
+})
