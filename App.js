ea37541de393a14b3587b13f9dac0a27f6204000
isaacdiniiz <isacpdalves@gmail.com>
2023-11-25 16:28:45 -0300

    New changer on style

--- App.js
+++ App.js
@@ -1,23 +1,49 @@
-import { StyleSheet, View, Image } from 'react-native';
+
+// Import necessary components from React Native
+import { StyleSheet, View, Image, Text } from 'react-native';
+
+// Import image from local assets
 import imagecat from './assets/imagecat.png';
 
+// Define the main App component
 export default function App() {
-  return (
+ // Return the JSX for the App component
+ return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Soletrar</Text>
+      </View>
       <Image source={imagecat} style={styles.image}/>
     </View>
-  );
+ );
 }
 
-  const styles = StyleSheet.create({
-    container: {
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    image: {
-      width: 267,
-      height: 267,
-      flexShrink: 0,
-      resizeMode: 'contain'
-    },
+// Define the styles for the App component
+const styles = StyleSheet.create({
+ container: {
+    justifyContent: 'center', // Center the content vertically
+    alignItems: 'center', // Center the content horizontally
+ },
+ image: {
+    width: 267, // Set the width of the image
+    height: 267, // Set the height of the image
+    flexShrink: 0,
+    marginTop: 50
+ },
+ header: {
+    backgroundColor: '#495E88',
+    width: 360,
+    height: 30,
+    flexShrink: 0,
+ },
+ headerText: {
+    flex: 1,
+    color: '#FFF',
+    fontFamily: 'inter',
+    fontSize: 24,
+    fontStyle: 'normal',
+    fontWeight: 700,
+    lineHeight: 24,
+    alignSelf: 'flex-start',
+ }
 })
