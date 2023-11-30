8f7555afd4f3618eec21c1d02f443732b10ec396
HelenaNotFunny <mh330413@gmail.com>
2023-11-29 22:41:07 -0300

    tentando resolver o problema da adaptabilidade
    não sei se deu certo meu expo app não tá carregando

--- App.js
+++ App.js
@@ -1,36 +1,90 @@
 
 // Import necessary components from React Native
-import { StyleSheet, View, Image, Text, useWindowDimensions } from 'react-native';
-// eu usei npm install --save react-native-dimension para instalar a biblioteca
-import { width, height, totalSize } from 'react-native-dimension';
+import { StyleSheet, View, Image, Text, TouchableOpacity, useWindowDimensions } from 'react-native';
 
+import { width, height } from 'react-native-dimension';
+//import Sound from 'react-native-sound';
 // Import image from local assets
 import imagecat from './assets/cat.png';
 import botaoVoltar from './assets/botao_voltar.png'
 import botaoSom from './assets/botao_som.png'
+import botaoReset from './assets/botao_reset.png'
 
-// Define the main App component
+const windowWidth = useWindowDimensions().width;
+const windowHeight = useWindowDimensions().height;
+
+function tecla(i){
+  return (
+    <TouchableOpacity>
+      <View style={styles.tecla}>
+        <Text style={styles.teclaLetra}>{alfabeto[i]}</Text>
+      </View>
+    </TouchableOpacity>
+  ) ;
+} ;
+
+//const audioGato = './assets/mixkit-sweet-kitty-meow-93.wav'
+
 export default function App() {
- // Return the JSX for the App component
  return (
   <View style={styles.container}>
       
     <View style={styles.header}>
-      <Image source={botaoVoltar} style={styles.headerVoltar}/>
+      <TouchableOpacity>
+        <Image source={botaoVoltar} style={styles.headerVoltar}/>
+      </TouchableOpacity>
       <Text style={styles.headerText}>Soletrar</Text>
     </View>
 
     <View style={styles.mid}>
       <Image source={imagecat} style={styles.image}/>
-      <Image source={botaoSom} style={styles.botaoSom}/>
+      <TouchableOpacity>
+        <Image source={botaoSom} style={styles.botaoSom}/>
+      </TouchableOpacity>
     </View>
 
     <View style={styles.slots}>
+      <View style={styles.slotCell} />
+      <View style={styles.slotCell} />
+      <View style={styles.slotCell} />
+      <View style={styles.slotCell} />
     </View>
 
     <View style={styles.keyboard}>
+      {tecla(0)}
+      {tecla(1)}
+      {tecla(2)}
+      {tecla(3)}
+      {tecla(4)}
+      {tecla(5)}
+      {tecla(6)}
+      {tecla(7)}
+      {tecla(8)}
+      {tecla(9)}
+      {tecla(10)}
+      {tecla(11)}
+      {tecla(12)}
+      {tecla(13)}
+      {tecla(14)}
+      {tecla(15)}
+      {tecla(16)}
+      {tecla(17)}
+      {tecla(18)}
+      {tecla(19)}
+      {tecla(20)}
+      {tecla(21)}
+      {tecla(22)}
+      {tecla(23)}
+      <View style={styles.invisibleBox}/>
+      {tecla(24)}
+      {tecla(25)}
+      <View style={styles.invisibleBox}>
+        <TouchableOpacity>
+          <Image source={botaoReset} style={styles.botaoReset}/>
+        </TouchableOpacity>
+      </View>
     </View>
-
+    
   </View>
  );
 }
@@ -45,41 +99,42 @@ const styles = StyleSheet.create({
  },
  header: {
   backgroundColor: 'rgba(73,	94,	136, 1)',
-  width: width(100),
-  height: height(10),
+  width: windowWidth,
+  height: windowHeight/10,
   flexShrink: 0,
   flexDirection: 'row'
 },
 headerVoltar: {
- marginTop: height(5.25),
+ marginTop: windowHeight*5.25/100,
  height: 26,
  width: 26,
  alignSelf: 'flex-start',
- marginLeft: width(5),// Center the footer text horizontally
+ marginLeft: windowWidth/20,// Center the footer text horizontally
 },
  headerText: {
   fontSize: 24, // Set the font size of the footer text
   fontWeight: 'bold',
-  lineHeight: height(7.25),
-  marginTop: height(3),
+  lineHeight: windowHeight*7.25/100,
+  marginTop: windowHeight*3/100,
   color: 'white', // Set the color of the footer text
   alignSelf: 'flex-start',
-  marginLeft: width(5), // Center the footer text horizontally
+  marginLeft: windowWidth/20, // Center the footer text horizontally
 },
  mid: {
   backgroundColor: 'white',
-  width: width(100),
-  height: height(40),
+  width: windowWidth,
+  height: windowHeight/2.5,
   flexShrink: 0,
   flexDirection: 'row',
   justifyContent: 'center', // Center the content vertically
   alignItems: 'center', // Center the content horizontally
 },
  image: {
-    width: 267, // Set the width of the image
-    height: 267, // Set the height of the image
+    width: windowWidth*3/4, // Set the width of the image
+    height: windowWidth*3/4, // Set the height of the imag >>>>>>> master
     flexShrink: 0,
     marginLeft: 26,
+    marginTop: windowHeight/50,
 },
  botaoSom: {
     width: 26, // Set the width of the image
@@ -89,11 +144,70 @@ headerVoltar: {
  slots: {
   backgroundColor: 'white',
   width: 420,
-  height: height(10),
+  height: windowHeight/10,
+  flexDirection: 'row',
+  justifyContent: 'center', // Center the content vertically
+  alignItems: 'center', // Center the content horizontally
+},
+ slotCell: {
+  backgroundColor: 'rgba(217, 217, 217, 1)',
+  width: 56,
+  height: 56,
+  borderTopLeftRadius: 10,
+  borderTopRightRadius: 10,
+  borderBottomWidth: 2,
+  borderColor: 'rgba(52, 152, 219, 1)',
+  marginHorizontal: 8,
 },
  keyboard: {
   backgroundColor: 'rgba(200, 215, 225, 1)',
   width: 420,
-  height: height(40),
+  height: windowHeight/2.5,
+  flexDirection: 'row',
+  justifyContent: 'center', 
+  alignContent: 'space-around',
+  flexWrap: 'wrap',
+  padding: 10,
+},
+tecla:{
+  backgroundColor: 'white',
+  width: 56,
+  height: 56,
+  borderRadius: 10,
+  borderWidth: 2,
+  borderColor: 'rgba(52, 152, 219, 1)',
+  shadowColor: 'black',
+  shadowOpacity: 0.3,
+  shadowRadius: 2,
+  shadowOffset: {width: 0,height:4},
+  justifyContent: 'center', // Center the content vertically
+  alignItems: 'center', // Center the content horizontally
+  marginHorizontal: 5,
+},
+teclaLetra:{
+  //fontFamily: "helvetica",
+  fontWeight: 'bold',
+  color: 'rgba(19, 2, 87, 1)',
+  fontSize: 30,
+},
+botaoReset: {
+  width: 36, // Set the width of the image
+  height: 36, // Set the height of the image
+  backgroundColor: 'white',
+  flexShrink: 0,
+  borderRadius: 17,
+  borderWidth: 2,
+  borderColor: 'rgba(52, 152, 219, 1)',
+  shadowColor: 'black',
+  shadowOpacity: 0.3,
+  shadowRadius: 2,
+  shadowOffset: {width: 0,height:4},
+},
+invisibleBox: {
+  width: 56,
+  height: 56,
+  justifyContent: 'center', // Center the content vertically
+  alignItems: 'center', // Center the content horizontally
+  marginHorizontal: 5,
 }
 })
